24d4de7e0bfac160fd4dc7a15fccb3e30fb0eb68
PLEASEWORK\Krzysztof <krzysztof.murczynski@gmail.com>
2017-08-12 13:32:32 +0200

    Trying to make movement happen...

---
+++ ETAPE/Assets/Scripts/CyclistsHandling/InputHandling.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EtapeGame
+{
+    namespace PlayerMovementHandler
+    {
+        public class InputHandling : MonoBehaviour {
+
+            public float turningSpeedMultiplayer;
+            public float DrivingImpulsForce;
+            private float horizontalInput;
+            public Rigidbody rb;
+            private Transform playerTransform;
+
+            void Start()
+            {
+                playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+                rb = playerTransform.gameObject.GetComponent<Rigidbody>();
+            }
+
+
+            void Update()
+            {
+                GetInput();
+            }
+
+            private void FixedUpdate()
+            {
+                TurningsHandling();
+            }
+
+            private void GetInput()
+            {
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    MovingForwardHandling();
+                }
+                horizontalInput = Input.GetAxis("Horizontal");
+            }
+
+            private void MovingForwardHandling()
+            { 
+                rb.AddForce(playerTransform.forward * DrivingImpulsForce, ForceMode.Impulse);
+            }
+
+            private void TurningsHandling()
+            {
+
+            }
+        }
+    }
+}
